e0f137a246ed3f8892caef9c60b18028648e1fa9
liuboheng <lbh9311@sina.com>
2019-06-09 18:42:21 +0800

    upgrade

--- client/src/containers/ariticle/index.tsx
+++ client/src/containers/ariticle/index.tsx
@@ -1,19 +1,18 @@
-import React from "react";
+import React from 'react'
 // import {observable} from 'mobx'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-import Header from "@components/header";
+import Header from '@components/header'
 
 class Article extends React.Component {
-  authorInfo={
+  authorInfo = {
     username: 'yeyan1996',
     avatar: ''
   }
   article = {
     time: 1560000347952,
-    viewCounts:10,
-    title:'大型项目前端架构浅谈（8000字原创）',
-
+    viewCounts: 10,
+    title: '大型项目前端架构浅谈（8000字原创）'
   }
   render() {
     return (
@@ -22,27 +21,27 @@ class Article extends React.Component {
         <main className="article-area">
           <article>
             <div className="author-info-block">
-              <Link to='/'><div className="avatar" /></Link>
+              <Link to="/">
+                <div className="avatar" />
+              </Link>
               <div className="author-info-block">
                 <div className="username">{this.authorInfo.username}</div>
                 <div className="meta-data">
                   <time>{this.article.time}</time>
                   <span className="view-counts">{this.article.viewCounts}</span>
                 </div>
-                <div className='actions'>
-                  <button className='follow'>关注</button>
+                <div className="actions">
+                  <button className="follow">关注</button>
                 </div>
               </div>
             </div>
             <h1>{this.article.title}</h1>
-            <div className="article-content">
-              
-            </div>
+            <div className="article-content" />
           </article>
         </main>
       </div>
-    );
+    )
   }
 }
 
-export default Article;
+export default Article
--- client/src/containers/home/index.tsx
+++ client/src/containers/home/index.tsx
@@ -1,38 +1,38 @@
-import React from "react";
-import { observer, inject } from "mobx-react";
+import React from 'react'
+import { observer, inject } from 'mobx-react'
 
-import Curtain from "@components/curtain";
-import Header from "@components/header";
-import EssayList from "@components/essayList";
-import NotePad from "@components/notePad";
+import Curtain from '@components/curtain'
+import Header from '@components/header'
+import EssayList from '@components/essayList'
+import NotePad from '@components/notePad'
 
-import RootStore from "@stores/root";
+import RootStore from '@stores/root'
 
-import "./index.less";
+import './index.less'
 
 interface Props {
-  root?: RootStore;
+  root?: RootStore
 }
 
-@inject("root")
+@inject('root')
 @observer
 class Home extends React.Component<Props> {
   handleNotePadDisplay = (flag: boolean) => () => {
     // FIXME: do not change state in component
-    this.props.root!.pop.notePad = flag;
-  };
+    this.props.root!.pop.notePad = flag
+  }
   render() {
-    const { notePad } = this.props.root!.pop;
+    const { notePad } = this.props.root!.pop
     return (
       <div className="home">
         <Curtain>
           <Header />
-          <EssayList title={"热门"} />
+          <EssayList title={'热门'} />
         </Curtain>
         {notePad && <NotePad onClose={this.handleNotePadDisplay(false)} />}
       </div>
-    );
+    )
   }
 }
 
-export default Home;
+export default Home
